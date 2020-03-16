f9978895b40de1d173286056f2c2ecaf75d67e49
Nazar Sabara <naz.sabara@gmail.com>
2020-03-17 00:22:32 +0200

    ML7 code review comments resolution

--- core-service/src/main/java/com/sabara/service/IntegrationServiceFallback.java
+++ core-service/src/main/java/com/sabara/service/IntegrationServiceFallback.java
@@ -2,16 +2,15 @@ package com.sabara.service;
 
 import com.sabara.model.resource.HeroResource;
 import feign.FeignException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 public class IntegrationServiceFallback implements IntegrationServiceClient {
 
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
     private final Throwable cause;
 
     public IntegrationServiceFallback(Throwable cause){
@@ -21,9 +20,9 @@ public class IntegrationServiceFallback implements IntegrationServiceClient {
     @Override
     public Optional<HeroResource> getHeroById(Long id) {
         if (cause instanceof FeignException && ((FeignException) cause).status() == 404){
-            logger.error("404 Resource not found.");
+            log.error("404 Resource not found.");
         } else {
-            logger.error("Something went wrong.");
+            log.error("Something went wrong.");
         }
 
         return Optional.empty();
@@ -31,7 +30,7 @@ public class IntegrationServiceFallback implements IntegrationServiceClient {
 
     @Override
     public List<HeroResource> getAllHeroes() {
-        logger.error("Something went wrong.");
+        log.error("Something went wrong.");
 
         return new ArrayList<>();
     }
